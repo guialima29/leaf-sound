3eb24b3ce205147598e649bddec00f7e7a2acd8c
GUILHERME AUGUSTO DE LIMA <07404626102@ifms.edu.br>
2025-08-18 22:39:08 -0400

    [UPDATE] - Change hooks approach

--- src/components/LoginArea.tsx
+++ src/components/LoginArea.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image"
-import { LoginWithGoogle } from "@/hooks/googleSignIn";
 import Link from "next/link";
 
 export default function LoginArea() {
     return (
         <div className="flex items-center justify-center flex-col gap-6">
             <div className="flex flex-row items-center justify-center gap-4 mt-20">
-                <Image src={'/lf-logo.svg'} alt="LeafSound" width={45} height={100}/>
+                <Image src={'/lf-logo.svg'} alt="LeafSound" width={45} height={100} />
                 <h1 className="font-bold font-libre-caslon text-[56px] text-[#2B3328]">Login</h1>
             </div>
-            <div>n
-                <LoginWithGoogle/>
+            <div>
+                <button 
+                    className="flex items-center justify-center gap-2 bg-white text-black px-4 py-2 rounded-lg shadow-md hover:bg-gray-100 transition-colors hover:cursor-pointer"
+                >
+                    <Image src="/google-icon.svg" width="48" height={48} alt="Google Icon" className="w-5 h-5" />
+                    <Link href="/workspace">Login com o Google</Link>
+                </button>
             </div>
             <p>Não tem uma conta?<Link href="/register" className="text-[#5DAA3B]"> Registre-se agora</Link></p>
         </div>
--- src/components/RegisterArea.tsx
+++ src/components/RegisterArea.tsx
@@ -1,21 +1,22 @@
 import Image from "next/image"
-import { ClerkProvider } from "@clerk/nextjs";
-import { RegisterWithGoogle } from "@/hooks/googleSignUp";
 import Link from "next/link";
 
 export default function LoginArea() {
     return (
-        <ClerkProvider>
-        <div className="flex items-center justify-center flex-col gap-6">
-            <div className="flex flex-row items-center justify-center gap-4 mt-20">
-                <Image src={'/lf-logo.svg'} alt="LeafSound" width={45} height={100}/>
-                <h1 className="font-bold font-libre-caslon text-[56px] text-[#2B3328]">Registrar-se</h1>
+            <div className="flex items-center justify-center flex-col gap-6">
+                <div className="flex flex-row items-center justify-center gap-4 mt-20">
+                    <Image src={'/lf-logo.svg'} alt="LeafSound" width={45} height={100} />
+                    <h1 className="font-bold font-libre-caslon text-[56px] text-[#2B3328]">Registrar-se</h1>
+                </div>
+                <div>
+                    <button
+                        className="flex items-center justify-center gap-2 bg-white text-black px-4 py-2 rounded-lg shadow-md hover:bg-gray-100 transition-colors hover:cursor-pointer"
+                    >
+                        <Image src="/google-icon.svg" width="48" height={48} alt="Google Icon" className="w-5 h-5" />
+                        <Link href="/workspace">Login com o Google</Link>
+                    </button>
+                </div>
+                <p>Já tem uma conta?<Link href="/login" className="text-[#5DAA3B]"> Logue agora</Link></p>
             </div>
-            <div>
-                <RegisterWithGoogle/>
-            </div>
-            <p>Já tem uma conta?<Link href="/login" className="text-[#5DAA3B]"> Logue agora</Link></p>
-        </div>
-        </ClerkProvider>
     )
 }
