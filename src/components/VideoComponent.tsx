7b1600e905edb282be7e9a2ec81cbf8bdbcd1119
GUILHERME AUGUSTO DE LIMA <07404626102@ifms.edu.br>
2025-08-18 22:38:26 -0400

    [FIX] - Pre render glitch update

--- src/components/VideoComponent.tsx
+++ src/components/VideoComponent.tsx
@@ -1,9 +1,9 @@
-export default async function VideoComponent() {
+export default function VideoComponent() {
   const src = "https://www.youtube.com/embed/T3sax7PB0vw?si=eGeFo0EE2J3aIPrQ";
  
   return ( 
     <div className="flex align-middle items-center justify-center">
-      <iframe width={1280} height={720} src={src} title="Apresentation Video" allowFullScreen />
+      <iframe width={1280} height={720} src={src} title="Apresentation Video" allowFullScreen suppressHydrationWarning />
     </div>
   )
 }
