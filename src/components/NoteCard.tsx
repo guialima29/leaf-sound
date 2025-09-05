28de7cf5098124ff081d9be0c46678736ec1e324
GUILHERME AUGUSTO DE LIMA <07404626102@ifms.edu.br>
2025-09-04 21:43:42 -0400

    [UPDATE] - Put map on GridWorkspace.tsx

--- src/components/NoteCard.tsx
+++ src/components/NoteCard.tsx
@@ -8,7 +8,7 @@ import {
     CardTitle,
 } from "@/components/ui/card"
 import Image from "next/image"
-import { componentsNotes } from "@/constants/constNoteCards"
+import Link from "next/link";
 import { Button } from "./ui/button"
 
 interface ComponentNote {
@@ -18,25 +18,23 @@ interface ComponentNote {
     lastUpdateTime: string;
 }
 
-export default function NoteCard() {
+export default function NoteCard(title: string, description: string, lastUpdateDate: string, lastUpdateTime:string, href: string, idx: number) {
     return (
-        <>
-            {componentsNotes.map((component: ComponentNote, idx: number) => (
-                <Card className="w-[300px] h-[360px]" key={idx}>
+                <Card className="w-[300px] h-[360px] relative" key={idx}>
                     <CardHeader>
                         <Image src="/note-img.png" alt="" width={300} height={200} />
                     </CardHeader>
                     <CardContent className="space-y-2">
-                        <CardTitle className="text-lg font-semibold">{component.title}</CardTitle>
-                        <p>{component.description}</p>
+                    <CardTitle className="text-lg font-semibold line-clamp-1">{title}</CardTitle>
+                        <p className="max-w-[300px] max-h-[75px] line-clamp-3">{description}</p>
                     </CardContent>
-                    <CardFooter className="flex flex-col space-y-4 ">
-                        <Button className="w-full">Acessar</Button>
-                        <p className="text-[14px]   ">Last update: {component.lastUpdateDate} at {component.lastUpdateTime}</p>    
+                    <CardFooter className="flex flex-col space-y-3 w-full absolute bottom-2 justify-center items-center ">
+                        <Link href={href}>
+                            <Button className="w-full">Acessar</Button>
+                        </Link>
+                        <p className="text-[14px] ">Last update: {lastUpdateDate} at {lastUpdateTime}</p>    
                     </CardFooter>
                 </Card>
-            ))}
-        </>
     )
 }
 
