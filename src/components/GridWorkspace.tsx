65adc53280f37cdac03334826153f78ad7069a45
GUILHERME AUGUSTO DE LIMA <07404626102@ifms.edu.br>
2025-09-04 21:44:05 -0400

    [UPDATE] - Add a map here (not complete)

--- src/components/GridWorkspace.tsx
+++ src/components/GridWorkspace.tsx
@@ -1,9 +1,21 @@
 import NoteCard from "./NoteCard";
+import { componentsNotes } from "@/constants/constNoteCards";
+
+interface ComponentNote {
+    title: string;
+    description: string;
+    lastUpdateDate: string;
+    lastUpdateTime: string;
+    href: string;
+}
 
 export default function GridWorkspace() {
     return(
         <div className="grid grid-cols-4 gap-20 p-4 content-center">
-            <NoteCard></NoteCard>
+            {componentsNotes.map((component, index) => (
+               // <NoteCard title={component.title} description={component.description} lastUpdateDate={component.lastUpdateDate} lastUpdateTime={component.lastUpdateTime} href={component.href} idx={index}/>
+            ))}
+            
         </div>
     )
 }
