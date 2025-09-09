bc4418fcb6057b30c095bca67c83a81ea4c5f7d4
GUILHERME AUGUSTO DE LIMA <07404626102@ifms.edu.br>
2025-09-08 22:18:20 -0400

    [UPDATE] - Align text on center

--- src/components/TitleWithBar.tsx
+++ src/components/TitleWithBar.tsx
@@ -1,7 +1,7 @@
 export default function TitleWithBar({title}:{title:string}) {
     return(
         <>
-            <div className="flex max-w-200 justify-center">
+            <div className="flex justify-center">
                 <h1 className="text-[36px] font-libre-caslon font-semibold text-[#2B3328]">{title}</h1>
             </div>
             <hr className="w-64 h-1 mx-auto my-4 bg-gray-300 border-0 rounded-sm md:my-10 dark:bg-gray-700"/>
