498f5024b8f6f8c44bf21923f4a02764c3193023
GUILHERME AUGUSTO DE LIMA <07404626102@ifms.edu.br>
2025-09-08 22:17:50 -0400

    [FIX/UPDATE] - Fix hidration error

--- src/components/VideoComponent.tsx
+++ src/components/VideoComponent.tsx
@@ -1,3 +1,46 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
+const DynamicVideoPlayer = dynamic(() => Promise.resolve(VideoPlayer), {
+  ssr: false,
+  loading: () => (
+    <div className="flex justify-center items-center">
+      <div className="w-full max-w-4xl aspect-video bg-gray-200 animate-pulse rounded-lg flex items-center justify-center">
+        <span>Carregando vídeo...</span>
+      </div>
+    </div>
+  )
+});
+
+function VideoPlayer() {
+  const src = "https://www.youtube.com/embed/T3sax7PB0vw?si=eGeFo0EE2J3aIPrQ";
+  
+  return ( 
+    <div className="flex justify-center items-center w-full">
+      <div className="relative w-full max-w-4xl aspect-video">
+        <iframe 
+          className="absolute inset-0 w-full h-full rounded-lg"
+          src={src} 
+          title="Apresentation Video" 
+          allowFullScreen 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default function VideoComponent() {
+  return <DynamicVideoPlayer />;
+}
+
+
+
+
+/* old method
+
+'use client';
 export default function VideoComponent() {
   const src = "https://www.youtube.com/embed/T3sax7PB0vw?si=eGeFo0EE2J3aIPrQ";
  
@@ -6,4 +49,4 @@ export default function VideoComponent() {
       <iframe width={1280} height={720} src={src} title="Apresentation Video" allowFullScreen suppressHydrationWarning />
     </div>
   )
-}
+}*/
