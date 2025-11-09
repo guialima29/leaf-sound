aac8ecc88be56b2f3bf8892199654629a29cc080
Guilherme Lima <pockte164@gmail.com>
2025-11-09 00:58:20 -0400

    [UPDATE] - Add new image

--- src/components/CreateNoteCard.tsx
+++ src/components/CreateNoteCard.tsx
@@ -1,15 +1,15 @@
 import {
     Card,
-    CardAction,
+    // CardAction,
     CardContent,
-    CardDescription,
+    // CardDescription,
     CardFooter,
-    CardHeader,
+    // CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-import Link from "next/link"
+// import Link from "next/link"
 import Image from "next/image"
-import { Button } from "./ui/button"
+// import { Button } from "./ui/button"
 
 export default function CreateNoteCard() {
     return (
@@ -17,7 +17,7 @@ export default function CreateNoteCard() {
             {/* <CardHeader>
             </CardHeader> */}
             <CardContent className="space-y-2">
-                <Image src="/note-img.png" alt="" width={300} height={200} />
+                <Image className="flex items-center justify-center" src="/plus-icon.png" alt="" width={100} height={100} />
             </CardContent>
             <CardFooter className="flex flex-col space-y-3 w-full absolute bottom-10 justify-center items-center ">
                 <CardTitle className="font-semibold line-clamp-1 text-center text-[24px]">Nova Anotação</CardTitle>
