2ae22ba83435687f591865ef5bdf93d79a61d596
GUILHERME AUGUSTO DE LIMA <07404626102@ifms.edu.br>
2025-11-27 21:15:20 -0400

    [NEW] - Created editor page and CSS file to style

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,12 +1,18 @@
+"use client"
+
 import Header from "@/components/Header";
-// import TextEditor from "@/components/TextEditor";
+import TextEditor from "@/components/TextEditor";
 import TitleWithBar from "@/components/TitleWithBar";
 
 export default function Editor() {
+    const handleChange = (data: any) => {
+        console.log("Data change: ", data)
+    }
     return (
         <>
-            <Header/>
-            <TitleWithBar title="Pop Chords"/>
+            <Header />
+            <TitleWithBar title="Pop Chords" />
+            <TextEditor onChange={handleChange} />
         </>
     )
 }
