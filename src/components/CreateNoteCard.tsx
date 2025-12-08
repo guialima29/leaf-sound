52f5da8454fb28df6c47a940af5effebae2c1f74
guialima29 <pockte164@gmail.com>
2025-12-08 16:27:53 -0400

    Remove comments...

--- src/components/CreateNoteCard.tsx
+++ src/components/CreateNoteCard.tsx
@@ -1,23 +1,22 @@
 import {
     Card,
-    // CardAction,
     CardContent,
-    // CardDescription,
     CardFooter,
-    // CardHeader,
     CardTitle,
 } from "@/components/ui/card"
-// import Link from "next/link"
 import Image from "next/image"
-// import { Button } from "./ui/button"
 
-export default function CreateNoteCard() {
+interface CreateNoteCardProps {
+    onClick?: () => void;
+}
+
+export default function CreateNoteCard({ onClick }: CreateNoteCardProps) {
     return (
-        <Card className="w-[300px] h-[360px] relative">
-            {/* <CardHeader>
-            </CardHeader> */}
-            <CardContent className="space-y-2">
-                <Image className="flex items-center justify-center" src="/plus-icon.png" alt="" width={100} height={100} />
+        <Card className="w-[300px] h-[360px] relative cursor-pointer hover:bg-slate-50 transition-colors" onClick={onClick}>
+            <CardContent className="space-y-2 pt-10">
+                <div className="flex items-center justify-center h-full">
+                    <Image className="flex items-center justify-center opacity-50" src="/plus-icon.png" alt="" width={100} height={100} />
+                </div>
             </CardContent>
             <CardFooter className="flex flex-col space-y-3 w-full absolute bottom-10 justify-center items-center ">
                 <CardTitle className="font-semibold line-clamp-1 text-center text-[24px]">Nova Anotação</CardTitle>
