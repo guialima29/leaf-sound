8c8078c2dcd9db054c568f809066057c83a2fb4a
Guilherme Lima <pockte164@gmail.com>
2026-02-26 20:10:29 -0400

    [NEW] - Added new sections and removed others

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,21 @@
 import Header from "@/components/Header";
-import HomeBeneficios from "@/components/HomeBeneficios";
 import HomeExemplo from "@/components/HomeExemplo";
 import HomeText from "@/components/HomeText";
 import Footer from "@/components/Footer";
 import VideoComponent from "@/components/VideoComponent";
+import { HomeFeatures } from "@/components/HomeFeatures";
+import { HomeHowWorks } from "@/components/HomeHowWorks";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-35 w-full">
+    <div className="flex flex-col gap-35 w-full min-h-screen">
       <Header />
-      <HomeText />
-      <VideoComponent />
-      <HomeBeneficios />
+      <div className="flex flex-row flex-grow w-full justify-center items-center gap-32">
+        <HomeText />
+        <VideoComponent />
+      </div>
+      <HomeFeatures/>
+      <HomeHowWorks/>
       <HomeExemplo />
       <Footer />
     </div>
