34f716503540b19c0d1e044e5596ced6f124a6ba
Guilherme Lima <pockte164@gmail.com>
2026-02-26 20:11:28 -0400

    [UPDATE] - Change header

--- src/components/HomeExemplo/index.tsx
+++ src/components/HomeExemplo/index.tsx
@@ -4,7 +4,14 @@ import CardHomeTemplate from "@/components/CardHomeTemplate"
 export default function HomeExemplo() {
     return (
         <div className="flex items-center flex-col justify-center min-h-screen w-full">
-            <h1 className="font-bold text-4xl text-[#2B3328] font-libre-caslon mb-15">Explore a sua criatividade musical 🧠🎵</h1>
+            <div className="mx-auto max-w-2xl text-center">
+          <span className="text-sm font-semibold uppercase tracking-widest text-primary">
+            Explore sua criatividade
+          </span>
+          <h2 className="mt-3 text-balance text-3xl font-bold tracking-tight text-foreground md:text-4xl">
+            Abra o coração e deixe a mente te guiar
+          </h2>
+        </div>
             <div className="relative">
                 <div>
                      <Image src={"/home/song-bg.png"} alt="Background" width={1025} height={571} className="block max-w-full h-auto"/>
